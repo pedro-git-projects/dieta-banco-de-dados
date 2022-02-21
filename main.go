1fabd0bb4237e5d23afbef10334fa4f8b0eb3556
pedro-git-projects <pedro.generic.email@gmail.com>
2022-02-21 13:14:29 -0300

    adding .env to protect db password

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func exec(db *sql.DB, sql string) sql.Result {
@@ -14,9 +17,18 @@ func exec(db *sql.DB, sql string) sql.Result {
 	return result
 }
 
+func environmentVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("%v: Could not load .env file", err)
+	}
+	return os.Getenv(key)
+}
+
 func main() {
+	password := environmentVariable("PASSWORD")
 	//  "driver" "user:password@/database"
-	db, err := sql.Open("mysql", "root:@/")
+	db, err := sql.Open("mysql", "root:"+password+"/")
 	if err != nil {
 		panic(err)
 	}
